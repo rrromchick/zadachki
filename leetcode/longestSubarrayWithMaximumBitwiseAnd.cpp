633f7b4bec57e61ad7cb3db9420749f8f70b8c37
rrromchick <nahorniyroman@gmail.com>
2025-07-30 12:41:57 +0300

    30.07

---
+++ leetcode/longestSubarrayWithMaximumBitwiseAnd.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+
+class Solution {
+public:
+    int longestSubarray(std::vector<int>& nums) {
+        if (nums.empty()) {
+            return 0;
+        }
+
+        int maxVal = 0;
+        for (const auto& num : nums) {
+            if (num > maxVal) {
+                maxVal = num;
+            }
+        }
+
+        int maxLen = 0;
+        int currentLen = 0;
+
+        for (const auto& num : nums) {
+            if (num == maxVal) {
+                ++currentLen;
+            } else {
+                maxLen = std::max(maxLen, currentLen);
+                currentLen = 0;
+            }
+        }
+
+        return std::max(currentLen, maxLen);
+    }
+};
